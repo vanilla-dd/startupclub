ba56f76fa55c906d4e38176291f6b49d49c6c682
vanilla-dd <akashsingh123456765432@gmail.com>
2025-01-17 22:23:05 +0530

    fix: fixes

--- src/components/feature.tsx
+++ src/components/feature.tsx
@@ -1,5 +1,4 @@
 import ButtonAnimated from "./animated/button-animated";
-import Gravity, { MatterBody } from "./animated/gravity";
 import Telegram from "./logos/telegram";
 import { Button } from "./ui/button";
 
@@ -52,87 +51,27 @@ const Feature = () => {
             </ButtonAnimated>
           </div>
         </div>
-        {/* <div className="flex w-full max-w-[600px] flex-col justify-between rounded-lg bg-yellow-200 transition-transform duration-300 will-change-transform hover:scale-[.99] md:rounded-xl">
-          <p className="px-6 py-6">Local Chapter</p>
-          <p className="text-center text-2xl lg:text-5xl">
-            "some text here..."
-          </p>
-          <div className="self-end px-4 py-4 lg:px-8 lg:py-6">
-            <ButtonAnimated>Start Now</ButtonAnimated>
-          </div>
-        </div> */}
         <div className="flex h-[300px] w-full max-w-[600px] flex-col rounded-xl bg-rose-100 md:rounded-xl lg:h-[500px] lg:rounded-3xl">
           <p className="px-6 py-6">Digital Footprint</p>
-          <div className="relative flex h-full w-full flex-col">
-            <Gravity gravity={{ x: 0, y: 1 }} className="h-full w-full">
-              <MatterBody
-                matterBodyOptions={{ friction: 0.5, restitution: 0.2 }}
-                x="10%"
-                y="33%"
-              >
-                <div className="rounded-full bg-gray-200 px-8 py-6 text-2xl hover:cursor-pointer sm:text-3xl">
-                  Meetup
-                </div>
-              </MatterBody>
-              <MatterBody
-                matterBodyOptions={{ friction: 0.5, restitution: 0.2 }}
-                x="20%"
-                y="10%"
-              >
-                <div className="rounded-full bg-[#0077b5] px-10 py-8 text-3xl text-white hover:cursor-pointer sm:text-4xl">
-                  Linkedin
-                </div>
-              </MatterBody>
-              <MatterBody
-                matterBodyOptions={{ friction: 0.5, restitution: 0.2 }}
-                x="70%"
-                y="40%"
-              >
-                <div className="rounded-full bg-gray-200 px-6 py-6 text-2xl hover:cursor-pointer sm:text-3xl">
-                  Youtube
-                </div>
-              </MatterBody>
-              <MatterBody
-                matterBodyOptions={{ friction: 0.5, restitution: 0.2 }}
-                x="60%"
-                y="50%"
-              >
-                <div className="rounded-full bg-gray-200 px-12 py-10 text-4xl hover:cursor-pointer sm:text-5xl">
-                  Discord
-                </div>
-              </MatterBody>
-              <MatterBody
-                matterBodyOptions={{ friction: 0.5, restitution: 0.2 }}
-                x="30%"
-                y="70%"
-              >
-                <div className="rounded-full bg-gray-200 px-6 py-6 text-2xl hover:cursor-pointer sm:text-3xl">
-                  Telegram
-                </div>
-              </MatterBody>
-              <MatterBody
-                matterBodyOptions={{ friction: 0.5, restitution: 0.2 }}
-                x="80%"
-                y="90%"
-              >
-                <div className="rounded-full bg-gray-200 px-6 py-6 text-2xl hover:cursor-pointer sm:text-3xl">
-                  Instagram
-                </div>
-              </MatterBody>
-            </Gravity>
-          </div>
+          <div className="relative flex h-full w-full flex-col"></div>
         </div>
 
         <div className="flex h-[300px] w-full max-w-[600px] flex-col justify-between rounded-lg bg-gray-200 transition-transform duration-300 will-change-transform hover:scale-[.99] md:rounded-xl lg:h-[500px]">
-          <p className="px-6 py-6">Take the Lead</p>
-          <div className="relative flex flex-1 flex-col items-center justify-center gap-4">
-            <Button className="px-10 py-8 text-2xl">Request mentorship</Button>
-            <Button className="px-10 py-8 text-2xl">
+          <p className="px-6 pb-3 pt-6 lg:py-6">Take the Lead</p>
+          <div className="relative flex flex-1 flex-col items-center justify-center gap-2 px-4 lg:gap-4 lg:px-6">
+            <Button className="self-start lg:px-10 lg:py-8 lg:text-2xl">
+              Request mentorship
+            </Button>
+            <Button className="self-end lg:px-10 lg:py-8 lg:text-2xl">
               Startup Pitch Workshop
             </Button>
-            <Button className="px-10 py-8 text-2xl">Give Interviewed</Button>
-            <Button className="px-10 py-8 text-2xl">Talent Registry</Button>
-            <Button className="px-10 py-8 text-2xl">Volenteer</Button>
+            <Button className="self-start lg:px-10 lg:py-8 lg:text-2xl">
+              Give Interviewed
+            </Button>
+            <Button className="self-end lg:px-10 lg:py-8 lg:text-2xl">
+              Talent Registry
+            </Button>
+            <Button className="lg:px-10 lg:py-8 lg:text-2xl">Volenteer</Button>
           </div>
         </div>
       </div>
